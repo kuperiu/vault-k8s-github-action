874ad6534633ac4a312b5fc5428deb5b83e261cb
Lior <lior@l01015153.lan>
2021-01-06 10:45:56 +0100

    v1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const jwt = fs.readFileSync('/var/run/secrets/eks.amazonaws.com/serviceaccount/token','utf8').replace("\n", "");
+const jwt = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token','utf8');
 const envVar = process.env.ENV_VAR
 const endpont = process.env.VAULT_ADDR;
 const namespace = process.env.VAULT_NAMESPACE;
@@ -8,7 +8,6 @@ const vaultKey = process.env.VAULT_KEY;
 const vaultField = process.env.VAULT_FIELD;
 
 const core = require('@actions/core');
-console.log(jwt)
 var options = {
     apiVersion: 'v1', 
     endpoint: endpont, 
