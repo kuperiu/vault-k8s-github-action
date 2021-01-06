bfdbac2a9b1e9268826ed8770ebd533102f9d225
Lior <lior@l01015153.lan>
2021-01-06 12:25:05 +0100

    test

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ const resp = vault.kubernetesLogin({  jwt: jwt, role: role})
 });
 
 Promise.resolve(resp).then((values) => {
-    core.setOutput("test", values.data[vaultField]);
+    core.setOutput(envVar, values.data[vaultField]);
 });
 
