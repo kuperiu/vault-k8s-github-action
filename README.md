787cdfb9888cb6d0508feab25a4b3d0b6b4084fb
Lior <lior@l01015153.lan>
2021-01-06 12:44:30 +0100

    test

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # vault-k8s-github-action
 
 ## Description
-A github action that enables you to fetch a vault secret using a K8S service account and export it to a chosen environment variable
+A github action that enables you to fetch a vault secret using a K8S service account and set it as an output
 
 ## Example
 ```yaml
     - name: Get Secret
+      id: secrets
       uses: kuperiu/vault-k8s-github-action@master
       env:
-        VAULT_ADDR: https://myvault.com
-        VAULT_NAMESPACE: mynamesapce
-        VAULT_ROLE: myvaultrole
-        VAULT_KEY: myvaultsecretkey
-        VAULT_FIELD: myvaultfield
-        ENV_VAR: myenvvar
+        vault_address: https://myvault.com
+        vault_namespace: mynamesapce
+        vault_role: myvaultrole
+        vault_key: myvaultsecretkey
+        vault_field: myvaultfield
+        secret_name: my_secret
+
+    - name: Output
+      run: echo "${{ steps.secrets.outputs.my_secret }}"
+
 ```
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const fs = require('fs')
 const jwt = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token','utf8');
 
-const endpont = core.getInput(vault_address);
-const namespace = core.getInput(vault_namespace);
-const role = core.getInput(vault_role);
-const vaultKey = core.getInput(vault_key);
-const vaultField = core.getInput(vault_field);
-const secretName = core.getInput(secret_name)
+const endpont = core.getInput('vault_address');
+const namespace = core.getInput('vault_namespace');
+const role = core.getInput('vault_role');
+const vaultKey = core.getInput('vault_key');
+const vaultField = core.getInput('vault_field');
+const secretName = core.getInput('secret_name')
 
 var options = {
     apiVersion: 'v1', 
@@ -28,6 +28,6 @@ const resp = vault.kubernetesLogin({  jwt: jwt, role: role})
 });
 
 Promise.resolve(resp).then((values) => {
-    core.setOutput(envVar, values.data[vaultField]);
+    core.setOutput(secretName, values.data[vaultField]);
 });
 
